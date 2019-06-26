9de673aa2c411aa72e181bb35eaec11515ad0b34
DEYEHE <jdeyehe@gmail.com>
2019-06-26 16:36:46 +0200

    add product

---
+++ BeaconBurger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerTerminal
+{
+    class BeaconBurger : IBurger
+    {
+
+        BeaconBurger burger ();
+        public IBurgerBuilder AddExtra(PortionSize portionSize, IExtra extra)
+        {
+        }
+
+        public IBurger GetBurger()
+        {
+
+            return this.burger;
+        }
+    }
+}
---
+++ Burger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerTerminal
+{
+    class Burger
+    {
+        private String type;
+        private Dictionary<PortionSize, IExtra> ingredients = new Dictionary<PortionSize, IExtra>();
+
+
+        public Burger(String type)
+        {
+            this.type = type;
+        }
+
+        public void AddIngredient(PortionSize size, IExtra ingredient)
+        {
+            ingredients.Add(size, ingredient);
+        }
+
+
+        public void Show()
+        {
+            foreach(var item in ingredients)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
