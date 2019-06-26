ab7e17e0b0deb21a2646a2501b8904e2770e469b
DEYEHE <jdeyehe@gmail.com>
2019-06-26 14:59:14 +0200

    Builder

---
+++ IBurgerBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerTerminal
+{
+    abstract class BurgerBuilder
+    {
+        protected Burger burger;
+
+        public Burger Burger
+        {
+            get { return burger; }
+        }
+
+
+        public abstract void AddBread();
+
+        public abstract void AddMeats();
+
+        public abstract void AddCheese();
+
+        public abstract void AddCondiments();
+
+        public abstract void AddVeggies();
+
+
+
+
+    }
+}
---
+++ MakeBurger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerTerminal
+{
+    class MakeBurger
+    {
+        public void MakeBurgerLine(BurgerBuilder burgerBuilder)
+        {
+            burgerBuilder.AddBread();
+            burgerBuilder.AddMeats();
+            burgerBuilder.AddCheese();
+            burgerBuilder.AddVeggies();
+            burgerBuilder.AddCondiments();
+
+        }
+
+    }
+}
